dd1a8505eea654b9d7a92044ba8c3b1f6056da75
goskelly <goskelly@gmail.com>
2024-01-30 00:12:01 +0000

    Added resources

--- MakingItBetterG55/src/application/Player.java
+++ MakingItBetterG55/src/application/Player.java
@@ -5,11 +5,20 @@ public class Player {
 	private char playerType;
 	private String playerName;
 	private boolean aiControlled;
+	private int moneyResource;
+	private int healthResource;
+	private int educationResource;
+	private int goodsResource;
 	
-	public Player(char playerType, String playerName, boolean aiControlled) {
+	public Player(char playerType, String playerName, boolean aiControlled, 
+			int moneyResource, int healthResource, int educationResource, int goodsResource) {
 		this.playerType = playerType;
 		this.playerName = playerName;
 		this.aiControlled = aiControlled;
+		this.moneyResource = moneyResource;
+		this.healthResource = healthResource;
+		this.educationResource = educationResource;
+		this.goodsResource = goodsResource;
 	}
 
 	public char getPlayerType() {
@@ -34,6 +43,22 @@ public class Player {
 
 	public void setAiControlled(boolean aiControlled) {
 		this.aiControlled = aiControlled;
+	}
+	
+	public int getMoneyResource() {
+		return moneyResource;
+	}
+	
+	public int getHealthResource() {
+		return healthResource;
+	}
+	
+	public int getEducationResource() {
+		return educationResource;
+	}
+	
+	public int getGoodsResource() {
+		return goodsResource;
 	}
 
 }
