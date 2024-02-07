b09c4b0f7c51cb62a338036d56c47d9876a72e7b
Zachary Inglis <zacharycinglis@gmail.com>
2024-02-07 05:20:36 -0600

    first branch commit, dice charge and shake with placeholder sprites

--- MakingItBetterG55/src/application/MainSceneHandler.java
+++ MakingItBetterG55/src/application/MainSceneHandler.java
@@ -1,22 +1,25 @@
 package application;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class MainSceneHandler {
-	
+
 	private int turnNumber = 0;
 	private boolean canMove = true;
 	private BoardHandler boardHandler;
-	
+
 	public void handleTurn() {
-		if(turnNumber > 0) {
+		if (turnNumber > 0) {
 			boardHandler.moveToNextPlayer(turnNumber % 4);
 		}
-		switch(turnNumber % 4) {
+		switch (turnNumber % 4) {
 		case 0:
 			break;
 		case 1:
@@ -28,48 +31,114 @@ public class MainSceneHandler {
 		}
 		turnNumber++;
 	}
-	
+
 	public void setCanMove(boolean canMove) {
 		this.canMove = canMove;
 	}
-	
+
 	public Scene makeMainScene() {
 		GridPane root = new GridPane();
 		Scene mainScene = new Scene(root, 1200, 800);
 		mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		
+
 		boardHandler = new BoardHandler(this);
 		Pane board = boardHandler.makeSquareBallGroup(200);
-		root.add(board,0,0);
+		root.add(board, 0, 0);
 		board.setTranslateX(550);
 		board.setTranslateY(400);
-		mainScene.addEventHandler(KeyEvent.KEY_RELEASED,(KeyEvent event)->{
+		mainScene.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
 
-                if (event.getCode() == KeyCode.A && canMove == true) {
-                	if(boardHandler.tryToMove(board, 0)) {
-                		canMove = false;
-                	}
-                }
-                if (event.getCode() == KeyCode.W && canMove == true) {
-                	if(boardHandler.tryToMove(board, 1)) {
-                		canMove = false;
-                	}
-                }
-                if (event.getCode() == KeyCode.D && canMove == true) {
-                	if(boardHandler.tryToMove(board, 2)) {
-                		canMove = false;
-                	}
-                }
-                if (event.getCode() == KeyCode.S && canMove == true) {
-                	if(boardHandler.tryToMove(board, 3)) {
-                		canMove = false;
-                	}
-                }
-                if (event.getCode() == KeyCode.P && canMove == true) {
-                	boardHandler.moveToNextPlayer(turnNumber % 4);
-                }
-            
-        });
+			if (event.getCode() == KeyCode.A && canMove == true) {
+				if (boardHandler.tryToMove(board, 0)) {
+					canMove = false;
+				}
+			}
+			if (event.getCode() == KeyCode.W && canMove == true) {
+				if (boardHandler.tryToMove(board, 1)) {
+					canMove = false;
+				}
+			}
+			if (event.getCode() == KeyCode.D && canMove == true) {
+				if (boardHandler.tryToMove(board, 2)) {
+					canMove = false;
+				}
+			}
+			if (event.getCode() == KeyCode.S && canMove == true) {
+				if (boardHandler.tryToMove(board, 3)) {
+					canMove = false;
+				}
+			}
+			if (event.getCode() == KeyCode.P && canMove == true) {
+				boardHandler.moveToNextPlayer(turnNumber % 4);
+			}
+
+		});
+
+		double originalBarX = 950;
+		double diceX = 950;
+		double diceY = 450;
+		
+		ImageView diceBar = new ImageView(new Image("/riceTile.png", 128, 32, false, false));
+		ImageView dice = new ImageView(new Image("/HouseTile.png", 128, 128, false, false));
+		diceBar.setVisible(false);
+
+		AnimationTimer diceBarAnimator = new AnimationTimer() {
+			private long lastUpdate;
+			double diceBarScaler = 0;
+			double translateX = 0;
+			double shakeFactor = 10;
+
+			@Override
+			public void start() {
+				lastUpdate = System.nanoTime();
+				translateX = originalBarX - 64;
+				diceBar.setScaleX(0);
+				diceBar.setVisible(true);
+				super.start();
+			}
+
+			@Override
+			public void handle(long now) {
+				double elaspedSeconds = (now - lastUpdate) / 1_000_000_000.0;
+				if ((diceBarScaler + elaspedSeconds) > 1) {
+					diceBarScaler = 1;
+					translateX = originalBarX;
+				} else {
+					translateX += (64 * elaspedSeconds);
+					diceBarScaler += elaspedSeconds;
+				}
+				dice.setTranslateX(diceX + ((Math.random() * shakeFactor * diceBarScaler) - (shakeFactor / 2 * diceBarScaler)));
+				dice.setTranslateY(diceY + ((Math.random() * shakeFactor * diceBarScaler) - (shakeFactor / 2 * diceBarScaler)));
+				diceBar.setScaleX(diceBarScaler);
+				diceBar.setTranslateX(translateX);
+				lastUpdate = now;
+			}
+
+			@Override
+			public void stop() {
+				diceBarScaler = 0;
+				diceBar.setVisible(false);
+				dice.setTranslateX(diceX);
+				dice.setTranslateY(diceY);
+				translateX = originalBarX;
+				super.stop();
+			}
+		};
+
+		dice.setOnMousePressed(e -> {
+			diceBarAnimator.start();
+		});
+		dice.setOnMouseReleased(e -> {
+			diceBarAnimator.stop();
+		});
+
+		root.add(dice, 0, 0);
+		root.add(diceBar, 0, 0);
+		dice.setTranslateX(diceX);
+		dice.setTranslateY(diceY);
+		diceBar.setTranslateX(originalBarX);
+		diceBar.setTranslateY(550);
+
 		return mainScene;
 	}
 }
