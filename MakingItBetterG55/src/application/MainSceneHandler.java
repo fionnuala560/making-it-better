ae593bdf64fb1113c51874ef63d4858a0a4f0fd1
Zachary Inglis <zacharycinglis@gmail.com>
2024-02-07 08:56:52 -0600

    dice rolls and end of turn now connected with movment

--- MakingItBetterG55/src/application/MainSceneHandler.java
+++ MakingItBetterG55/src/application/MainSceneHandler.java
@@ -8,11 +8,15 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class MainSceneHandler {
 
 	private int turnNumber = 0;
-	private boolean canMove = true;
+	private int movement = 0;
+	private boolean canMove = false;
 	private BoardHandler boardHandler;
 
 	public void handleTurn() {
@@ -32,8 +36,15 @@ public class MainSceneHandler {
 		turnNumber++;
 	}
 
-	public void setCanMove(boolean canMove) {
-		this.canMove = canMove;
+	public void landedNextTurn() {
+	}
+	
+	public void landed() {
+		if(movement > 0) {
+			this.canMove = true;
+		} else {
+			handleTurn();
+		}
 	}
 
 	public Scene makeMainScene() {
@@ -46,33 +57,13 @@ public class MainSceneHandler {
 		root.add(board, 0, 0);
 		board.setTranslateX(550);
 		board.setTranslateY(400);
-		mainScene.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
-
-			if (event.getCode() == KeyCode.A && canMove == true) {
-				if (boardHandler.tryToMove(board, 0)) {
-					canMove = false;
-				}
-			}
-			if (event.getCode() == KeyCode.W && canMove == true) {
-				if (boardHandler.tryToMove(board, 1)) {
-					canMove = false;
-				}
-			}
-			if (event.getCode() == KeyCode.D && canMove == true) {
-				if (boardHandler.tryToMove(board, 2)) {
-					canMove = false;
-				}
-			}
-			if (event.getCode() == KeyCode.S && canMove == true) {
-				if (boardHandler.tryToMove(board, 3)) {
-					canMove = false;
-				}
-			}
-			if (event.getCode() == KeyCode.P && canMove == true) {
-				boardHandler.moveToNextPlayer(turnNumber % 4);
-			}
-
-		});
+		
+		Text movementText = new Text(Integer.toString(movement));
+		movementText.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+		
+		root.add(movementText, 0, 0);
+		movementText.setTranslateX(1000);
+		movementText.setTranslateY(350);
 
 		double originalBarX = 950;
 		double diceX = 950;
@@ -130,6 +121,9 @@ public class MainSceneHandler {
 		});
 		dice.setOnMouseReleased(e -> {
 			diceBarAnimator.stop();
+			movement = (int) (Math.random() * 6) + 1;
+			canMove = true;
+			movementText.setText(Integer.toString(movement));
 		});
 
 		root.add(dice, 0, 0);
@@ -138,6 +132,43 @@ public class MainSceneHandler {
 		dice.setTranslateY(diceY);
 		diceBar.setTranslateX(originalBarX);
 		diceBar.setTranslateY(550);
+		
+		mainScene.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
+
+			if (event.getCode() == KeyCode.A && canMove == true) {
+				int changeInMovement = boardHandler.tryToMove(board, 0, movement);
+				if (changeInMovement != -1) {
+					canMove = false;
+					movement -= changeInMovement;
+					movementText.setText(Integer.toString(movement));
+				}
+			}
+			if (event.getCode() == KeyCode.W && canMove == true) {
+				int changeInMovement = boardHandler.tryToMove(board, 1, movement);
+				if (changeInMovement != -1) {
+					canMove = false;
+					movement -= changeInMovement;
+					movementText.setText(Integer.toString(movement));
+				}
+			}
+			if (event.getCode() == KeyCode.D && canMove == true) {
+				int changeInMovement = boardHandler.tryToMove(board, 2, movement);
+				if (changeInMovement != -1) {
+					canMove = false;
+					movement -= changeInMovement;
+					movementText.setText(Integer.toString(movement));
+				}
+			}
+			if (event.getCode() == KeyCode.S && canMove == true) {
+				int changeInMovement = boardHandler.tryToMove(board, 3, movement);
+				if (changeInMovement != -1) {
+					canMove = false;
+					movement -= changeInMovement;
+					movementText.setText(Integer.toString(movement));
+				}
+			}
+
+		});
 
 		return mainScene;
 	}
