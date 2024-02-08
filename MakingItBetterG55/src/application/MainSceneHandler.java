11cedf49645fda73671828c962d25a5bc0aa4184
fionnuala560 <fionnuala560@gmail.com>
2024-02-08 14:22:54 +0000

    Updated to handle conflicts with Main. Changed the size and position of the ball. Changed the position of the Dice, Change Turn button and Label to fit with the player panels. Player panels now rotate when End Turn button is clicked

--- MakingItBetterG55/src/application/MainSceneHandler.java
+++ MakingItBetterG55/src/application/MainSceneHandler.java
@@ -1,18 +1,21 @@
 package application;
 
 import javafx.animation.AnimationTimer;
-import javafx.geometry.Rectangle2D;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+
 
 public class MainSceneHandler {
 
@@ -24,6 +27,7 @@ public class MainSceneHandler {
 	private ImageView dice = new ImageView();
 	private Text diceText = new Text("Click To Roll!");
 	private Button endTurnButton = new Button("End Turn");
+	int[] currentPlayerIndex = {0};
 
 	public void handleTurn() {
 		if (turnNumber > 0) {
@@ -59,27 +63,31 @@ public class MainSceneHandler {
 	}
 
 	public Scene makeMainScene() {
-		GridPane root = new GridPane();
+		BorderPane root  = new BorderPane();
+		GridPane grid = new GridPane();
+		root.setCenter(grid);
 		Scene mainScene = new Scene(root, 1200, 800);
 		mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 		boardHandler = new BoardHandler(this);
-		Pane board = boardHandler.makeSquareBallGroup(200);
-		root.add(board, 0, 0);
-		board.setTranslateX(550);
-		board.setTranslateY(400);
+		Pane board = boardHandler.makeSquareBallGroup(150);
+		grid.add(board, 0, 0);
+		board.setTranslateX(500);
+		board.setTranslateY(300);
 
 		diceText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-		root.add(diceText, 0, 0);
-		diceText.setTranslateX(950);
-		diceText.setTranslateY(390);
+		grid.add(diceText, 0, 0);
+		diceText.setTranslateX(850);
+		diceText.setTranslateY(190);
 
 		endTurnButton.setStyle(
 				"-fx-cursor: hand; -fx-border-color: #152546; -fx-border-width: 10px; -fx-background-color: #536F7B; -fx-text-fill: white;");
 		endTurnButton.setFont(Font.font("SansSerif", FontWeight.BOLD, 18));
 		endTurnButton.setPrefSize(128, 48);
 		endTurnButton.setVisible(false);
+		endTurnButton.setTranslateX(850);
+		endTurnButton.setTranslateY(200);
 
 		endTurnButton.setOnAction(event -> {
 			if (canMove && endTurnButton.isVisible()) {
@@ -93,8 +101,8 @@ public class MainSceneHandler {
 		});
 
 		double originalBarX = 950;
-		double diceX = 950;
-		double diceY = 450;
+		double diceX = 850;
+		double diceY = 250;
 
 		ImageView diceBarBorder = new ImageView(new Image("/diceBarBorder.png", 128, 32, false, false));
 		ImageView diceBar = new ImageView(new Image("/diceBar.png", 128, 32, false, false));
@@ -212,18 +220,20 @@ public class MainSceneHandler {
 			}
 		});
 
-		root.add(dice, 0, 0);
-		root.add(diceBar, 0, 0);
-		root.add(diceBarBorder, 0, 0);
-		root.add(endTurnButton, 0, 0);
+		grid.add(dice, 0, 0);
+		grid.add(diceBar, 0, 0);
+		grid.add(diceBarBorder, 0, 0);
+		grid.add(endTurnButton, 0, 0);
 		dice.setTranslateX(diceX);
 		dice.setTranslateY(diceY);
 		diceBar.setTranslateX(originalBarX);
 		diceBar.setTranslateY(550);
 		diceBarBorder.setTranslateX(originalBarX);
 		diceBarBorder.setTranslateY(550);
-		endTurnButton.setTranslateX(originalBarX);
-		endTurnButton.setTranslateY(560);
+		endTurnButton.setTranslateX(850);
+		endTurnButton.setTranslateY(350);
+
+
 
 		mainScene.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
 
@@ -242,9 +252,151 @@ public class MainSceneHandler {
 
 		});
 
+		Player Engineer = new Player('e', "Homer", false);
+		Player Teacher = new Player('t', "Marge", false);
+		Player Parent = new Player('p', "Bart", false);
+		Player Student = new Player('s', "Lisa", false);
+
+		Player[] allPlayers  = new Player[]{Engineer, Teacher, Parent, Student};
+		endTurnButton.setOnAction(event -> switchPlayer(allPlayers, root));
+
+		HBox bottomPlayerBox = createPlayerPanel(allPlayers[0]);
+		VBox rightPlayerBox = createPlayerPanelSides(allPlayers[1]);
+		HBox topPlayerBox = createPlayerPanel(allPlayers[2]);
+		VBox leftPlayerBox = createPlayerPanelSides(allPlayers[3]);
+
+		formatBottomPanel(bottomPlayerBox, root);
+		formatRightPanel(rightPlayerBox, root);
+		formatTopPanel(topPlayerBox, root);
+		formatLeftPanel(leftPlayerBox, root);
+
 		return mainScene;
 	}
 
+	private void switchPlayer(Player[] allPlayers, BorderPane root){
+		currentPlayerIndex[0] = (currentPlayerIndex[0]+1)%4;
+		HBox newBottomPlayerBox = createPlayerPanel(allPlayers[currentPlayerIndex[0]]);
+		VBox newRightPlayerBox = createPlayerPanelSides(allPlayers[(currentPlayerIndex[0] + 1) % 4]);
+		HBox newTopPlayerBox = createPlayerPanel(allPlayers[(currentPlayerIndex[0] + 2) % 4]);
+		VBox newLeftPlayerBox = createPlayerPanelSides(allPlayers[(currentPlayerIndex[0] + 3) % 4]);
+
+		formatBottomPanel(newBottomPlayerBox, root);
+		formatRightPanel(newRightPlayerBox, root);
+		formatTopPanel(newTopPlayerBox, root);
+		formatLeftPanel(newLeftPlayerBox,root);
+	}
+
+
+
+	private void formatLeftPanel(VBox box, BorderPane root){
+		VBox.setVgrow(root, Priority.ALWAYS);
+		box.setMaxHeight(500);
+		box.setPrefWidth(70);
+		root.setLeft(box);
+		BorderPane.setAlignment(box, Pos.CENTER_LEFT);
+	}
+
+	private void formatRightPanel(VBox box, BorderPane root){
+		VBox.setVgrow(root, Priority.ALWAYS);
+		box.setMaxHeight(500);
+		box.setPrefWidth(70);
+		root.setRight(box);
+		BorderPane.setAlignment(box, Pos.CENTER_RIGHT);
+	}
+
+	private void formatTopPanel(HBox box, BorderPane root){
+		box.setPrefHeight(60);
+		box.setMaxWidth(500);
+		root.setTop(box);
+		BorderPane.setAlignment(box, Pos.TOP_CENTER);
+	}
+
+	private void formatBottomPanel(HBox box, BorderPane root){
+		HBox.setHgrow(root, Priority.ALWAYS);
+		box.setMaxWidth(1000);
+		box.setPrefHeight(100);
+		root.setBottom(box);
+		BorderPane.setAlignment(box, Pos.BOTTOM_CENTER);
+	}
+
+	private HBox createPlayerPanel(Player currentPlayer){
+
+		String backgroundColor = getPlayerColor(currentPlayer.getPlayerType());
+		String borderColor = getBorderColor(currentPlayer.getPlayerType());
+
+		//creates a horizontal box (for the top/bottom of the screen
+		HBox playerPanel = new HBox();
+		playerPanel.setStyle("-fx-background-color: " +  backgroundColor + ";-fx-border-color: " + borderColor + ";-fx-border-width: 5px;");
+
+
+		playerPanel.setPadding(new Insets(5));
+
+		//creates a label for each player resource
+		Label nameLabel = new Label(currentPlayer.getPlayerName());
+		Label scoreLabel = new Label(Integer.toString(currentPlayer.getscore()));
+		Label moneyLabel = new Label(Integer.toString(currentPlayer.getMoney()));
+		Label goodsLabel = new Label(Integer.toString(currentPlayer.getGoods()));
+		Label educationsLabel = new Label(Integer.toString(currentPlayer.getEducation()));
+		Label healthLabel = new Label(Integer.toString(currentPlayer.getHealth()));
+
+		//adds all of the player resources to the player panel box
+		playerPanel.getChildren().addAll(nameLabel, scoreLabel, moneyLabel, goodsLabel, educationsLabel, healthLabel);
+
+		return playerPanel;
+	}
+
+	private VBox createPlayerPanelSides(Player currentPlayer){
+
+		String backgroundColor = getPlayerColor(currentPlayer.getPlayerType());
+		String borderColor = getBorderColor(currentPlayer.getPlayerType());
+		//creates a vertical box (for the left/right of the screen)
+		VBox playerPanel = new VBox();
+		playerPanel.setStyle("-fx-background-color: " +  backgroundColor + ";-fx-border-color: " + borderColor + ";-fx-border-width: 5px;");
+		playerPanel.setPadding(new Insets(5));
+
+		//creates a label for each player resource
+		Label nameLabel = new Label(currentPlayer.getPlayerName());
+		Label scoreLabel = new Label(Integer.toString(currentPlayer.getscore()));
+		Label moneyLabel = new Label(Integer.toString(currentPlayer.getMoney()));
+		Label goodsLabel = new Label(Integer.toString(currentPlayer.getGoods()));
+		Label educationsLabel = new Label(Integer.toString(currentPlayer.getEducation()));
+		Label healthLabel = new Label(Integer.toString(currentPlayer.getHealth()));
+
+		//adds all of the player resources to the player panel box
+		playerPanel.getChildren().addAll(nameLabel, scoreLabel, moneyLabel, goodsLabel, educationsLabel, healthLabel);
+
+		return playerPanel;
+	}
+
+	private String getPlayerColor(char playerType){
+		switch(playerType){
+		case 'e':
+			return "lightskyblue";
+		case 't':
+			return "lightgreen";
+		case 'p':
+			return "lightpink";
+		case 's':
+			return "mediumpurple";
+		}
+
+		return null;
+	}
+
+	private String getBorderColor(char playerType){
+		switch(playerType){
+		case 'e':
+			return "darkblue";
+		case 't':
+			return "darkgreen";
+		case 'p':
+			return "deeppink";
+		case 's':
+			return "indigo";
+		}
+
+		return null;
+	}
 	private void processMoveInput(Pane board, int direction) {
 		int changeInMovement = boardHandler.tryToMove(board, direction, movement);
 		if (changeInMovement != -1) {
@@ -259,4 +411,5 @@ public class MainSceneHandler {
 			}
 		}
 	}
+
 }
