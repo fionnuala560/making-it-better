a921aef7fe89d228a0ff86fb8d6792283812d1a1
fionnuala560 <fionnuala560@gmail.com>
2024-02-09 14:23:08 +0000

    Added methods to the player class to add to the score in the event of completing a task, taking steps to achieve objectives and maintaining resources through the game

--- MakingItBetterG55/src/application/Player.java
+++ MakingItBetterG55/src/application/Player.java
@@ -105,4 +105,99 @@ public class Player {
 		return score;
 	}
 
+	/*
+	Student objective 2 going to school a number of times
+	Teacher objective 2 and 3 eat certain number of apples, teach at school a number of time
+	Parent objective 2 and 3 buy certain number of goods from shop, interact with student
+	Engineer objective 2 and 3 install certain no. improvements for school, train teacher
+	 */
+
+	//to be called every time a player makes a step to complete an objective
+	public void contributeToObjective(){
+		this.score += 5;
+	}
+
+	//each time a player completes a task that they had ownership of
+	public void completeTask(){
+		this.score += 20;
+	}
+
+	//each time a player contributes resources to a task that is not their own
+	public void contributeResource(){
+		this.score += 5;
+	}
+
+	//scoreMaintainResource() and scoreExcessResources() to be called only when the game is finished
+
+	//calculated for each player on game over
+	public void scoreMaintainResource(){
+		if(this.playerType == 's'){
+			if(this.health >= (studentStartingRes[1])*0.75){
+				this.score += 20;
+			}
+		} else if(this.playerType == 't'){
+			if(this.education >= (teacherStartingRes[2])*0.75){
+				this.score += 20;
+			}
+		} else if(this.playerType == 'p'){
+			if(this.goods >= (parentStartingRes[3])*0.75){
+				this.score += 20;
+			}
+		} else if(this.playerType == 'e'){
+			if(this.money >= (engineerStartingRes[0])*0.75){
+				this.score += 20;
+			}
+		}
+	}
+
+	//student specific for objective achieved
+	public void scoreStudentReachedResource(){
+		if(this.playerType == 's'){
+			if(this.education >= studentStartingRes[2]*2){
+				this.score += 20;
+			}
+		}
+	}
+
+	//calculated for each player on game over
+	public void scoreExcessResources(){
+		if(this.playerType == 's'){
+			if(this.money >= (studentStartingRes[0])*0.25){
+				this.score += 10;
+			} /*else if(this.education >= (studentStartingRes[2])*0.25){
+				this.score += 10;
+			} */else if(this.goods >= (studentStartingRes[3])*0.25){
+				this.score += 10;
+			}
+		} else if(this.playerType == 't'){
+			if(this.money >= (teacherStartingRes[0])*0.25){
+				this.score += 10;
+			} else if(this.health >= (teacherStartingRes[1])*0.25){
+				this.score += 10;
+			} else if(this.goods >= (teacherStartingRes[3])*0.25){
+				this.score += 10;
+			}
+		} else if(this.playerType == 'p'){
+			if(this.money >= (parentStartingRes[0])*0.25){
+				this.score += 10;
+			} else if(this.education >= (parentStartingRes[2])*0.25){
+				this.score += 10;
+			} else if(this.health >= (parentStartingRes[1])*0.25){
+				this.score += 10;
+			}
+		} else if(this.playerType == 'e'){
+			if(this.health >= (engineerStartingRes[1])*0.25){
+				this.score += 10;
+			} else if(this.education >= (engineerStartingRes[2])*0.25){
+				this.score += 10;
+			} else if(this.goods >= (engineerStartingRes[3])*0.25){
+				this.score += 10;
+			}
+		}
+	}
+
+
+
+
+
 }
