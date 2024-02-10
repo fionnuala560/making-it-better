51320907b9f45c52ac32ef87a0b9dc5861083348
mkell01 <mkelly011001@gmail.com>
2024-02-10 22:20:12 +0000

    Beginning to set up epilogue Screen.

--- MakingItBetterG55/src/application/EpilogueScreen.java
+++ MakingItBetterG55/src/application/EpilogueScreen.java
@@ -24,19 +24,19 @@ public class EpilogueScreen {
     private Label label3a = new Label("An attempt was made.");
     private Label label4a = new Label("Yikes...");
 
-    private Label label1b = new Label("All objectives were successfully completed,\nyou worked very well together as a team");
-    private Label label2b = new Label("Most of the objectives were completed,\nthis was a good starting point");
-    private Label label3b = new Label("Some of the objectives were completed,\nmaybe next time try to work more as a team");
-    private Label label4b = new Label("Very few of the objectives were completed,\nmaybe someone else can fix what you've done");
+    private Label label1b = new Label("All objectives were successfully completed,\n   you worked very well together as a team");
+    private Label label2b = new Label("Most of the objectives were completed,\n       this was a good starting point");
+    private Label label3b = new Label("   Some of the objectives were completed,\nmaybe next time try to work more as a team");
+    private Label label4b = new Label("  Very few of the objectives were completed");
 
-    private Label label1c = new Label("A greater level of student progression is shown in the coming years,\n as more children stay in school");
-    private Label label2c = new Label("More children are gaining a greater level of education,\nhowever there is still more work to be done.");
-    private Label label3c = new Label("Some children are staying in school,\n but more will have to be done to truly make a long lasting difference");
-    private Label label4c = new Label("Very little difference has been made for the people of Pu Ngaol,\n maybe someone else will be able to put this plan into effect more successfully");
+    private Label label1c = new Label("A greater level of student progression is shown\n         in the coming years, as more children\n                             stay in school");
+    private Label label2c = new Label("More children are gaining a greater level of\n     education, however there is still more\n                       work to be done.");
+    private Label label3c = new Label("         Some children are staying in school,\n but more will have to be done to truly make a\n                   long lasting difference");
+    private Label label4c = new Label("Little difference has been made for the people\nof Pu Ngaol, maybe someone else will be able\n to put this plan into effect more successfully");
 
     private Label label1d = new Label("No notes for future attempts.");
     private Label label2d = new Label("Make sure to keep an eye on your resources!");
-    private Label label3d = new Label("Keep your objectives in mind as you progress through the game.");
+    private Label label3d = new Label("Keep your objectives in mind as you progress\n                        through the game.");
     private Label label4d = new Label("Try working together to complete the tasks?");
 
     private int currentLabelIndex = 0;
@@ -74,7 +74,7 @@ public class EpilogueScreen {
         quitButton.setFont(Font.font("SansSerif", FontWeight.BOLD, 40));
         quitButton.setStyle("-fx-cursor: hand; -fx-border-color: #152546; -fx-border-width: 14px; -fx-background-color: #536F7B; -fx-text-fill: white");
         quitButton.setPrefSize(320, 29);
-        quitButton.setTranslateX(-175);
+        quitButton.setTranslateX(-475);
         quitButton.setTranslateY(500);
         epilogue.setRight(quitButton);
         quitButton.setOnAction(event -> {
@@ -86,13 +86,38 @@ public class EpilogueScreen {
         Label[] anAttemptArr = {label3a, label3b, label3c, label3d};
         Label[] yikesArr = {label4a, label4b, label4c, label4d};
 
+        for (Label x : fullWinArr){
+            x.setFont(Font.font("SansSerif", FontWeight.BOLD, 36));
+            x.setStyle("-fx-fill: black");
+        }
+
+        for(Label x : goodTryArr){
+            x.setFont(Font.font("SansSerif", FontWeight.BOLD, 36));
+            x.setStyle("-fx-fill: black");
+        }
+
+        for (Label x : anAttemptArr){
+            x.setFont(Font.font("SansSerif", FontWeight.BOLD, 36));
+            x.setStyle("-fx-fill: black");
+        }
+
+        for (Label x : yikesArr){
+            x.setFont(Font.font("SansSerif", FontWeight.BOLD, 36));
+            x.setStyle("-fx-fill: black");
+        }
+
         VBox epilogueText = new VBox();
-        epilogueText.setStyle("-fx-border-width: 5px; -fx-border-color: black");
+        //epilogueText.setStyle("-fx-border-width: 5px; -fx-border-color: black");
         epilogueText.setPadding(new Insets(5));
         epilogueText.setTranslateY(-100);
+        epilogueText.setTranslateX(-150);
         epilogueText.setMaxHeight(400);
-        epilogueText.setPrefWidth(950);
+        epilogueText.setMinWidth(875);
         epilogueText.getChildren().addAll(fullWinArr);
+        //epilogueText.getChildren().addAll(goodTryArr);
+        //epilogueText.getChildren().addAll(anAttemptArr);
+        //epilogueText.getChildren().addAll(yikesArr);
+        epilogueText.setAlignment(Pos.CENTER);
         for(Label i : fullWinArr){
             i.setVisible(false);
         }
@@ -104,6 +129,39 @@ public class EpilogueScreen {
                 }
             }
         });
+        /*for(Label i : goodTryArr){
+            i.setVisible(false);
+        }
+        epilogueScene.setOnKeyPressed(event ->{
+            if(event.getCode() == KeyCode.N){
+                if(currentLabelIndex < goodTryArr.length){
+                    goodTryArr[currentLabelIndex].setVisible(true);
+                    currentLabelIndex++;
+                }
+            }
+        });*/
+        /*for(Label i : anAttemptArr){
+            i.setVisible(false);
+        }
+        epilogueScene.setOnKeyPressed(event ->{
+            if(event.getCode() == KeyCode.N){
+                if(currentLabelIndex < anAttemptArr.length){
+                    anAttemptArr[currentLabelIndex].setVisible(true);
+                    currentLabelIndex++;
+                }
+            }
+        });*/
+        /*for(Label i : yikesArr){
+            i.setVisible(false);
+        }
+        epilogueScene.setOnKeyPressed(event ->{
+            if(event.getCode() == KeyCode.N){
+                if(currentLabelIndex < yikesArr.length){
+                    yikesArr[currentLabelIndex].setVisible(true);
+                    currentLabelIndex++;
+                }
+            }
+        });*/
         epilogue.setCenter(epilogueText);
     }
 
