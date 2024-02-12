a8e118d2062af09a97be097fcb07eb0739b66400
fionnuala560 <fionnuala560@gmail.com>
2024-02-12 16:20:41 +0000

    Fixed scoring methods to work with new resources array Added listener that automatically adds 20 to their score when the Student player doubles their education stat Changed score from int to IntegerProperty so that it can update with the UI

--- MakingItBetterG55/src/application/Player.java
+++ MakingItBetterG55/src/application/Player.java
@@ -1,56 +1,72 @@
 package application;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Player {
-	
-	private char playerType; //s = student, p = parent, t = teacher, e = engineer
+
+	private char playerType; // s = student, p = parent, t = teacher, e = engineer
 	private String playerName;
 	private boolean isAIControlled;
-	private int money;
-	private int health;
-	private int education;
-	private int goods;
-	private int score = 0;
-	private static int[] studentStartingRes = {1, 2, 3, 4};
-	private static int[] parentStartingRes = {1, 2, 3, 4};
-	private static int[] teacherStartingRes = {1, 2, 3, 4};
-	private static int[] engineerStartingRes = {1, 2, 3, 4};
-	
+	// indexed: health, education, goods, money
+	private int[] resources = {-1, -1, -1, -1};
+	private int[] objectives = { 0, 0, 0 };
+	private static int[] studentStartingRes = { 100, 10, 30, 0 };
+	private static int[] parentStartingRes = { 100, 40, 50, 30 };
+	private static int[] teacherStartingRes = { 100, 70, 50, 30 };
+	private static int[] engineerStartingRes = { 100, 80, 50, 80 };
+
+	private IntegerProperty score = new SimpleIntegerProperty(0);
+
 	public Player(char playerType, String playerName, boolean isAIControlled) {
 		this.playerType = playerType;
 		this.playerName = playerName;
 		this.isAIControlled = isAIControlled;
-		switch(playerType) {
-		case 's':
-			money = studentStartingRes[0]; 
-			health = studentStartingRes[1];
-			education = studentStartingRes[2];
-			goods = studentStartingRes[3];
-			break;
-		case 'p':
-			money = parentStartingRes[0]; 
-			health = parentStartingRes[1];
-			education = parentStartingRes[2];
-			goods = parentStartingRes[3];
-			break;
-		case 't':
-			money = teacherStartingRes[0]; 
-			health = teacherStartingRes[1];
-			education = teacherStartingRes[2];
-			goods = teacherStartingRes[3];
-			break;
-		case 'e':
-			money = engineerStartingRes[0]; 
-			health = engineerStartingRes[1];
-			education = engineerStartingRes[2];
-			goods = engineerStartingRes[3];
-			break;
-		default:
-			money = -1;
-			health = -1;
-			education = -1;
-			goods = -1;
-			
+		switch (playerType) {
+			case 's':
+				resources[0] = studentStartingRes[0];
+				resources[1] = studentStartingRes[1];
+				resources[2] = studentStartingRes[2];
+				resources[3] = studentStartingRes[3];
+				break;
+			case 'p':
+				resources[0] = parentStartingRes[0];
+				resources[1] = parentStartingRes[1];
+				resources[2] = parentStartingRes[2];
+				resources[3] = parentStartingRes[3];
+				break;
+			case 't':
+				resources[0] = teacherStartingRes[0];
+				resources[1] = teacherStartingRes[1];
+				resources[2] = teacherStartingRes[2];
+				resources[3] = teacherStartingRes[3];
+				break;
+			case 'e':
+				resources[0] = engineerStartingRes[0];
+				resources[1] = engineerStartingRes[1];
+				resources[2] = engineerStartingRes[2];
+				resources[3] = engineerStartingRes[3];
+				break;
 		}
+
+		initListeners();
+	}
+
+	//automatically adds 20 to the score if the player is a Student and their education has at least doubled from its starting value
+	private void addStudentEducationListener() {
+		if (playerType == 's') {
+			int initialEducation = resources[1];
+
+			score.addListener((observable, oldValue, newValue) -> {
+				if (resources[1] >= initialEducation * 2) {
+					increaseScore(20);
+				}
+			});
+		}
+	}
+
+	private void initListeners(){
+		addStudentEducationListener();
 	}
 
 	public char getPlayerType() {
@@ -64,88 +80,103 @@ public class Player {
 	public boolean isAIControlled() {
 		return isAIControlled;
 	}
-	
+
 	public int getMoney() {
-		return money;
+		return resources[3];
 	}
-	
+
 	public void setMoney(int money) {
-		this.money = money;
+		resources[3] = money;
 	}
-	
+
 	public int getHealth() {
-		return health;
+		return resources[0];
 	}
-	
+
 	public void setHealth(int health) {
-		this.health = health;
+		resources[0] = health;
 	}
-	
+
 	public int getEducation() {
-		return education;
+		return resources[1];
 	}
-	
+
 	public void setEducation(int education) {
-		this.education = education;
+		resources[1] = education;
 	}
-	
+
 	public int getGoods() {
-		return goods;
+		return resources[2];
 	}
-	
+
 	public void setGoods(int goods) {
-		this.goods = goods;
+		resources[2] = goods;
 	}
-	
-	public void setScore(int score) {
-		this.score = score;
+
+	public int[] getResources() {
+		return resources;
 	}
-	
-	public int getscore() {
+
+	public int[] getObjectives() {
+		return objectives;
+	}
+
+	public IntegerProperty scoreProperty() {
 		return score;
+	}
+
+	public int getScore() {
+		return score.get();
+	}
+
+	public void setScore(int score) {
+		this.score.set(score);
+	}
+
+	private void increaseScore(int n){
+		score.set(score.get() + n);
 	}
 
 	/*
 	Student objective 2 going to school a number of times
-	Teacher objective 2 and 3 eat certain number of apples, teach at school a number of time
+	Teacher objective 2 and 3 eat certain number of apples, teach at school a number of times
 	Parent objective 2 and 3 buy certain number of goods from shop, interact with student
 	Engineer objective 2 and 3 install certain no. improvements for school, train teacher
 	 */
 
 	//to be called every time a player makes a step to complete an objective
 	public void contributeToObjective(){
-		this.score += 5;
+		score.set(score.get() + 5);
 	}
 
 	//each time a player completes a task that they had ownership of
 	public void completeTask(){
-		this.score += 20;
+		score.set(score.get() + 20);
 	}
 
 	//each time a player contributes resources to a task that is not their own
 	public void contributeResource(){
-		this.score += 5;
+		score.set(score.get() + 5);
 	}
 
 	//scoreMaintainResource() and scoreExcessResources() to be called only when the game is finished
-
 	//calculated for each player on game over
 	public void scoreMaintainResource(){
 		if(this.playerType == 's'){
-			if(this.health >= (studentStartingRes[1])*0.75){
-				this.score += 20;
+			if(this.resources[0] >= (studentStartingRes[1])*0.75){
+				score.set(score.get() + 20);
 			}
 		} else if(this.playerType == 't'){
-			if(this.education >= (teacherStartingRes[2])*0.75){
-				this.score += 20;
+			if(this.resources[1] >= (teacherStartingRes[2])*0.75){
+				score.set(score.get() + 20);
 			}
 		} else if(this.playerType == 'p'){
-			if(this.goods >= (parentStartingRes[3])*0.75){
-				this.score += 20;
+			if(this.resources[2] >= (parentStartingRes[3])*0.75){
+				score.set(score.get() + 20);
 			}
 		} else if(this.playerType == 'e'){
-			if(this.money >= (engineerStartingRes[0])*0.75){
-				this.score += 20;
+			if(this.resources[3] >= (engineerStartingRes[0])*0.75){
+				score.set(score.get() + 20);
 			}
 		}
 	}
@@ -153,8 +184,8 @@ public class Player {
 	//student specific for objective achieved
 	public void scoreStudentReachedResource(){
 		if(this.playerType == 's'){
-			if(this.education >= studentStartingRes[2]*2){
-				this.score += 20;
+			if(this.resources[1] >= studentStartingRes[2]*2){
+				score.set(score.get() + 20);
 			}
 		}
 	}
@@ -162,42 +193,41 @@ public class Player {
 	//calculated for each player on game over
 	public void scoreExcessResources(){
 		if(this.playerType == 's'){
-			if(this.money >= (studentStartingRes[0])*0.25){
-				this.score += 10;
-			} /*else if(this.education >= (studentStartingRes[2])*0.25){
-				this.score += 10;
-			} */else if(this.goods >= (studentStartingRes[3])*0.25){
-				this.score += 10;
+			if(this.resources[3] >= (studentStartingRes[0])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[2] >= (studentStartingRes[3])*0.25){
+				score.set(score.get() + 10);
 			}
 		} else if(this.playerType == 't'){
-			if(this.money >= (teacherStartingRes[0])*0.25){
-				this.score += 10;
-			} else if(this.health >= (teacherStartingRes[1])*0.25){
-				this.score += 10;
-			} else if(this.goods >= (teacherStartingRes[3])*0.25){
-				this.score += 10;
+			if(this.resources[3] >= (teacherStartingRes[0])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[0] >= (teacherStartingRes[1])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[2] >= (teacherStartingRes[3])*0.25){
+				score.set(score.get() + 10);
 			}
 		} else if(this.playerType == 'p'){
-			if(this.money >= (parentStartingRes[0])*0.25){
-				this.score += 10;
-			} else if(this.education >= (parentStartingRes[2])*0.25){
-				this.score += 10;
-			} else if(this.health >= (parentStartingRes[1])*0.25){
-				this.score += 10;
+			if(this.resources[3] >= (parentStartingRes[0])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[1] >= (parentStartingRes[2])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[0] >= (parentStartingRes[1])*0.25){
+				score.set(score.get() + 10);
 			}
 		} else if(this.playerType == 'e'){
-			if(this.health >= (engineerStartingRes[1])*0.25){
-				this.score += 10;
-			} else if(this.education >= (engineerStartingRes[2])*0.25){
-				this.score += 10;
-			} else if(this.goods >= (engineerStartingRes[3])*0.25){
-				this.score += 10;
+			if(this.resources[0] >= (engineerStartingRes[1])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[1] >= (engineerStartingRes[2])*0.25){
+				score.set(score.get() + 10);
+			} else if(this.resources[2] >= (engineerStartingRes[3])*0.25){
+				score.set(score.get() + 10);
 			}
 		}
 	}
 
-
-
-
+	public void addGameOverScores(){
+		this.scoreExcessResources();
+		this.scoreMaintainResource();
+	}
 
 }
